f120840b890d6c2689c4149e28398009a6e63890
“[Nicol <[nicol.saha@gmail.com]>
2020-09-19 19:46:18 +0200

    practice

--- E6_NS.php
+++ E6_NS.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 
+
 class Beverage
 {
     private string $color;
@@ -9,22 +10,32 @@ class Beverage
 
     private const BARNAME = 'Het Vervolg';
 
+    private static int $timesServed = 0;
+
+
     public function __construct(string $color, float $price, string $temperature = 'Cold')
     {
         $this->color = $color;
         $this->price = $price;
         $this->temperature = $temperature;
+        self::$timesServed++;
+    }
+
+    public function getTimesServed () {
+        return  '<br/> Total number of drinks served: ' . self::$timesServed;
     }
 
     public function getBarName() {
-    return self::BARNAME;
+        return self::BARNAME;
     }
 
     public function getInfo(): string
     {
-        return "Colour:  " . $this->color . "<br/> Price: € " . $this->price . "<br/> Barname:  " . $this->getBarName() . "<br/> Temperature: " . $this->temperature ;
+        return "Colour:  " . $this->color . "<br/> Price: € " . $this->price . "<br/> Barname:  " . $this->getBarName() . "<br/> Temperature: " . $this->temperature;
     }
+
 }
+
 
 class Beer extends Beverage
 {
@@ -45,15 +56,24 @@ class Beer extends Beverage
     }
 
     private function beerInfo(): string {
-        return 'Name: ' . $this ->name . '<br/> Alcohol %: ' . $this->alcoholpercentage  . '<br/>Country: ' . $this->getBeerCountry();
+        return 'Name: ' . $this ->name . '<br/> Alcohol %: ' . $this->alcoholpercentage  . '<br/>Country: ' . $this->getBeerCountry()  ;
     }
 
     public function displayBeerInfo(): string {
         return $this -> beerInfo();
     }
 
+
+
 }
 
 $duvel = new Beer('Duvel', 8.5, 'blond', 3.50, $temperature = "cold");
+$beer = new Beer('Beer', 0.0, 'beercolor', 0.0, $temperature = "cold");
+
+
 echo $duvel->getInfo() . "<br>";
 echo $duvel->displayBeerInfo() . "<br>";
+echo "<br>";
+echo $beer->getInfo() . "<br>";
+echo $beer->displayBeerInfo() . "<br>";
+echo $duvel->getTimesServed();
